18875695e3e69705450485e075b6b84a7413d010
HAMA-IJAZ <56930936+HAMA-IJAZ@users.noreply.github.com>
2020-06-14 00:09:24 +0500

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,6 @@ export const Header = () => {
   return (
     <h2>
        Exploring how to edit files required files that is needed is"app.css" and app.js and index.js just  
-        <br>and for a good practice divide a progarm into smaller module.</br>
-        Create folder aganist each module then import in app.js file  </h2>
+       </h2>
   )
 }
