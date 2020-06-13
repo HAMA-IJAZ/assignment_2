195b77c7cff17f6bc3076d59b8393cd7e5fea94f
HAMA-IJAZ <56930936+HAMA-IJAZ@users.noreply.github.com>
2020-06-13 23:23:46 +0500

    simpley add  header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Header } from './components/Header';
 import './App.css';
 
-
 function App() {
-  let [count, setcount] = useState(1)
-  let [isMorning, setMorning] = useState(false)
   return (
-    <div className="App">
-      <h1>To Quite From Bad Habbit </h1><br>
-      <h2>Its Important To Make Up Your Mind </h2>
-      <h3>So Lets Do A Simple Exercise </h3>
-      <header className={`App-header ${isMorning ? 'daylight' : ''}`}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3> {isMorning ? "Good Morning Saad Ali" : "Good Night Saad Ali"}</h3>
-        
-        
-        <button onClick={() => setMorning(!isMorning)}>Update Day & Night</button>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      
+    </GlobalProvider>
   );
 }
 
